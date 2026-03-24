162898c4d0f8ddc27f230c9b97e1308b7e4d3bb1
Christian Escabillas <christianescabillas44@gmail.com>
2026-03-24 14:19:46 +0200

    Add review page

--- app.py
+++ app.py
@@ -12,6 +12,40 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_item")
+def new_item():
+    return render_template("new_item.html")
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    item_type = request.form["item_type"]
+
+    sql = "INSERT INTO item (title, item_type) VALUES (?, ?)"
+    db.execute(sql, [title, item_type])
+
+    return redirect("/")
+
+@app.route("/new_review")
+def new_review():
+    items = db.query("SELECT id, title FROM item")
+    return render_template("new_review.html", items=items)
+
+@app.route("/create_review", methods=["POST"])
+def create_review():
+    title = request.form["title"]
+    thoughts = request.form["thoughts"]
+    rating = request.form["rating"]
+    item_id = request.form["item_id"]
+    user_id = session["user_id"]
+
+    sql = "INSERT INTO review (title, thoughts, rating, user_id, item_id) VALUES (?, ?, ?, ?, ?)"
+    db.execute(sql, [title, thoughts, rating, user_id, item_id])
+    
+    return redirect("/")
+
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -43,10 +77,13 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash =  result["password_hash"]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -55,4 +92,5 @@ def login():
 @app.route("/logout")
 def logout():
     del session["username"]
+    del session["user_id"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -4,3 +4,41 @@ CREATE TABLE users (
     password_hash TEXT
 );
 
+CREATE TABLE item (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    item_type TEXT CHECK(item_type IN ('movie', 'series', 'game', 'song'))
+);
+
+CREATE TABLE review (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    thoughts TEXT,
+    rating INTEGER,
+    user_id INTEGER REFERENCES users,
+    item_id INTEGER REFERENCES item
+);
+
+CREATE TABLE movie (
+    id INTEGER PRIMARY KEY,
+    movie_title TEXT,
+    release_year INTEGER
+);
+
+CREATE TABLE series (
+    id INTEGER PRIMARY KEY,
+    series_title TEXT,
+    release_year INTEGER
+);
+
+CREATE TABLE game (
+    id INTEGER PRIMARY KEY,
+    game_name TEXT,
+    release_year INTEGER
+);
+
+CREATE TABLE song (
+    id INTEGER PRIMARY KEY,
+    song_title TEXT,
+    singer TEXT
+);
